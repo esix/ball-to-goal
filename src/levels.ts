99f6c6e011f7fe5c147e833a5dcc4d26d2b56452
esix <esix@live.ru>
2025-12-26 17:15:41 +0300

    Added levels build from string

--- src/levels.ts
+++ src/levels.ts
@@ -1,49 +1,91 @@
-import { Direction, LevelData, PipeType } from "./utils";
+import { Direction, FIELD_HEIGHT, FIELD_WIDTH, LevelData, PipeType } from "./utils";
 
-const level1 = `
-...╭.╮.......
-.............
-...╰.╯.......
+/**
+ * Creates level information from multiline string
+ * @param s
+ */
+function makeLevel(s: string): LevelData {
+  const rows = s.split('\n').slice(1).filter(row => !!row);
+  if (rows.length === FIELD_HEIGHT) throw new Error('Incorrect level height');
+  if (!rows.every(row => row.length === FIELD_WIDTH))  throw new Error('Incorrect level width');
 
-`;
+  let cannon: { col: number; row: number; direction: Direction } | null = null;
+  let goal: { col: number; row: number } | null = null;
+  const pipes: Array<{ type: PipeType; col: number; row: number }> = [];
+  const walls: Array<{ col: number; row: number }> = [];
+  const pits: Array<{ col: number; row: number }> = [];
 
+  for (let row = 0; row < FIELD_HEIGHT; row++) {
+    for (let col = 0; col < FIELD_WIDTH; col++) {
+      const c = rows[row][col];
 
-const LEVELS: LevelData[] = [
-  {
-    cannon: { col: 1, row: 4, direction: Direction.Right },
-    goal: { col: 6, row: 1 },
-    pipes: [
-      { col: 1, row: 3, type: PipeType.LeftUp },
-    ],
-    walls: [
-      { col: 5, row: 2 }
-    ],
-    pits: [
-      { col: 10, row: 2 }, { col: 11, row: 2 }, { col: 12, row: 2 }, { col: 13, row: 2 },
-      { col: 10, row: 3 }, { col: 11, row: 3 },
-      { col: 10, row: 4 }, { col: 11, row: 4 },
-    ],
-  },
-  {
-    cannon: { col: 1, row: 4, direction: Direction.Right },
-    goal: { col: 9, row: 3 },
-    pipes: [
-      { col: 3, row: 0, type: PipeType.RightDown },    { col: 5, row: 0, type: PipeType.LeftDown },
-      { col: 3, row: 2, type: PipeType.RightUp },      { col: 5, row: 4, type: PipeType.LeftUp },
-    ],
-    walls: [ { col: 9, row: 4 }, { col: 9, row: 2 }],
-    pits: [],
-  },
-  {
-    cannon: { col: 4, row: 4, direction: Direction.Left },
-    goal: { col: 9, row: 4 },
-    pipes: [
-      { col: 6, row: 3, type: PipeType.RightDown },    { col: 7, row: 3, type: PipeType.LeftDown },
-      { col: 7, row: 4, type: PipeType.RightUp },      { col: 6, row: 4, type: PipeType.LeftUp },
-    ],
-    walls: [],
-    pits: [],
-  },
+      if (c === '·') {}
+
+      else if (c === '←') cannon = { row, col, direction: Direction.Left };
+      else if (c === '→') cannon = { row, col, direction: Direction.Right };
+      else if (c === '↓') cannon = { row, col, direction: Direction.Down };
+      else if (c === '↑') cannon = { row, col, direction: Direction.Up };
+
+      else if (c === '╮') pipes.push({row, col, type: PipeType.LeftDown});
+      else if (c === '╭') pipes.push({row, col, type: PipeType.RightDown});
+      else if (c === '╯') pipes.push({row, col, type: PipeType.LeftUp});
+      else if (c === '╰') pipes.push({row, col, type: PipeType.RightUp});
+
+      else if (c === '○') goal = { row, col };
+
+      else if (c === '▲') walls.push({ row, col });
+
+      else if (c === '~') pits.push({ row, col });
+    }
+  }
+
+  if (!cannon) throw new Error('No cannon');
+  if (!goal) throw new Error('No cannon');
+
+  return {
+    cannon,
+    goal,
+    pipes,
+    walls,
+    pits,
+  }
+}
+
+const X_LEVELS = [
+  ` LEVEL 0
+··············
+······○·······
+·····▲····~~~~
+·╯········~~··
+·→········~~··
+··············
+··············
+··············
+··············`,
+
+  ` LEVEL 1
+···╭·╮········
+··············
+···╰·····▲····
+·········○····
+·→···╯···▲····
+··············
+··············
+··············
+··············`,
+
+  ` LEVEL 2
+··············
+··············
+··············
+······╭╮······
+····←·╯╰·○····
+··············
+··············
+··············
+··············`,
 ];
+
+const LEVELS: LevelData[] = X_LEVELS.map(makeLevel);
 
 export default LEVELS;
